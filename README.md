6964bc323544b33ac723c8c97d808cfc1be27255
yanutetsu <satoshi.yanuma@topgate.co.jp>
2017-04-12 01:06:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# angular-toggle-counterpart
+
+Easily toggle countarpart files (.ts|.spec.ts|.html) in angular.
+
+## Usage
+
+- `ctrl+c`
+  - open `.html` if you are opening `.ts`.
+  - open `.ts` if you are opening `.html`.
+- `ctrl+shift+c`
+  - open `.spec.ts` if you are opening `.ts`.
+  - open `.ts` if you are opening `.spec.ts`.  
+
+## Release Notes
+
+### 0.0.1
+
+Initial release
+
+## Source
+
+[GitHub](https://github.com/yanutetsu/angular-toggle-couterpart.git)
---
+++ src/extension.ts
@@ -0,0 +1,84 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('Congratulations, your extension "angular-toggle-counterpart" is now active!');
+
+    let disposableToggleCounterpart = vscode.commands.registerCommand('extension.toggleCounterpart', () => {
+        const filePath: string = vscode.window.activeTextEditor.document.fileName;
+
+        if (!isAngularFile(filePath)) {
+            return;
+        }
+
+        const path: PathStr = splitByExtension(filePath);
+
+        if (!(path.ext === 'ts' || path.ext === 'html')) {
+            return;
+        }
+
+        const counterpart: string = path.base + (path.ext === 'ts' ? '.html' : '.ts');
+
+        vscode.workspace.openTextDocument(counterpart)
+            .then((document: vscode.TextDocument) => {
+                vscode.window.showTextDocument(document);
+            });
+    });
+
+    let disposableToggleCounterpartOfTesting = vscode.commands.registerCommand('extension.toggleCounterpartTesting', () => {
+        const filePath: string = vscode.window.activeTextEditor.document.fileName;
+
+        if (!isAngularFile(filePath)) {
+            return;
+        }
+        
+        const path: PathStr = splitByExtension(filePath);
+
+        if (path.ext !== 'ts') {
+            return;
+        }
+
+        const extend: PathStr = splitByExtension(path.base);
+
+        const counterpart: string = extend.ext === 'spec' ? extend.base + '.ts' : path.base + '.spec.ts';
+
+        vscode.workspace.openTextDocument(counterpart)
+            .then((document: vscode.TextDocument) => {
+                vscode.window.showTextDocument(document);
+            });
+    })
+
+    context.subscriptions.push(disposableToggleCounterpart);
+    context.subscriptions.push(disposableToggleCounterpartOfTesting);
+}
+
+function isAngularFile(filepath: string): boolean {
+    return isAngularFileType(filepath, 'component')
+        || isAngularFileType(filepath, 'service')
+        || isAngularFileType(filepath, 'directive')
+        || isAngularFileType(filepath, 'pipe');
+}
+
+function isAngularFileType(filepath: string, type: string): boolean {
+    return 0 <= filepath.indexOf(type);
+}
+
+function splitByExtension(filepath: string): PathStr {
+    const splittedfilepath: string[] = filepath.split('.');
+    const ext: string = splittedfilepath.pop();
+
+    return <PathStr>{
+        base: splittedfilepath.join('.'),
+        ext: ext
+    };
+}
+
+interface PathStr {
+    base: string;
+    ext: string;
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
